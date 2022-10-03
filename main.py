dc172b5a4a3b784c2bdf6178e64cc1b1c516d2ab
AingCodes <spowza@gmail.com>
2022-10-03 19:30:11 +0000

    lights

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='.', intents=intents, help_command=None)
 
-try:
-  token = getenv('TOKEN')
-except:
-  token = jsonfuncs.get_from_db('bot_token.json', 'TOKEN')
-
 @bot.event
 async def on_ready():
   print('Graham Bot is online.')
 
 async def main():
   async with bot:
+    try:
+      token = getenv('TOKEN')
+    except:
+      token = jsonfuncs.get_from_db('bot_token.json', 'TOKEN')
+      
     await bot.load_extension('yahtzeecommands')
     await bot.load_extension('misccommands')
     await bot.load_extension('bjcommands')
