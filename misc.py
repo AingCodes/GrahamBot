73b8c77a20d39e68269fadf3c8a06d0b6393d041
GrahamMcPhee <brvwsbvwseb@gmail.com>
2022-10-05 02:20:56 +0100

    added a function to make buttons more easily

--- misc.py
+++ misc.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 def get_name(user):
@@ -8,3 +9,16 @@ def cvt_member(ctx, tag):
   converter = commands.MemberConverter()
   member = converter.convert(ctx, tag)
   return member
+
+def create_buttons(**kwargs):
+  view = discord.ui.View()
+  items = [
+    discord.ui.Button(
+    custom_id=kwargs['ids'][i],
+    label=kwargs['labels'][i] if kwargs['labels'] else None,
+    emoji=kwargs['emojis'][i] if kwargs['emojis'] else None,
+  ) for i, x in enumerate(kwargs['ids'])
+  ]
+  for item in items:
+    view.add_item(item)
+  return view
